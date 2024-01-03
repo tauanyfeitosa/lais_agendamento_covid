93363679f01c6dde14c4c5ce7f4a2642e24d1c6e
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2024-01-03 12:55:55 -0300

    feat (autenticacao): implementação da aptidão para agendamento e modelo GrupoAtendimentoCandidato

--- autenticacao/models.py
+++ autenticacao/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
@@ -14,7 +16,7 @@ class BaseUser(AbstractBaseUser, PermissionsMixin):
         verbose_name='CPF'
     )
     is_active = models.BooleanField(
-        default=False
+        default=True
     )
     is_superuser = models.BooleanField(
         default=False
@@ -39,7 +41,7 @@ class BaseUser(AbstractBaseUser, PermissionsMixin):
 
 class Candidato(BaseModel):
     candidato = models.OneToOneField(
-        BaseUser,
+        'BaseUser',
         on_delete=models.PROTECT,
         verbose_name=u'Usuário'
     )
@@ -57,11 +59,36 @@ class Candidato(BaseModel):
 
     @property
     def idade(self):
-        pass
+        hoje = date.today()
+        idade = hoje.year - self.data_nascimento.year
+
+        if (hoje.month, hoje.day) < (self.data_nascimento.month, self.data_nascimento.day):
+            idade -= 1
+
+        return idade
 
     @property
     def eh_apto_agendamento(self):
-        pass
+        if self.covid_30_dias:
+            return False
+
+        if self.idade < 18:
+            return False
+
+        nomes_grupos_nao_permitidos = [
+            'População Privada de Liberdade',
+            'Pessoas com Deficiência Institucionalizadas',
+            'Pessoas ACAMADAS de 80 anos ou mais'
+        ]
+
+        grupos_nao_permitidos = self.grupos_atendimento.filter(
+            grupo_atendimento__nome__in=nomes_grupos_nao_permitidos
+        )
+
+        if grupos_nao_permitidos.exists():
+            return False
+
+        return True
 
     def __str__(self):
         return self.nome
@@ -69,3 +96,21 @@ class Candidato(BaseModel):
     class Meta:
         verbose_name = 'Candidato'
         verbose_name_plural = 'Candidatos'
+
+
+class GrupoAtendimentoCandidato(BaseModel):
+    candidato = models.ForeignKey(
+        'Candidato',
+        on_delete=models.PROTECT,
+        verbose_name='Candidato',
+        related_name='grupos_atendimento'
+    )
+    grupo_atendimento = models.ForeignKey(
+        'base.GrupoAtendimento',
+        on_delete=models.PROTECT,
+        verbose_name='Grupo de Atendimento',
+        related_name='candidatos'
+    )
+
+    def __str__(self):
+        return f'{self.candidato} - {self.grupo_atendimento}'
