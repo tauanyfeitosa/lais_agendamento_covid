aecb3d3c9e83f1b17ffb23454543cc89939165c8
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2024-01-03 12:58:36 -0300

    feat (home): view e rota para exibir informações do candidato

---
+++ base/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from base.views import home
+
+app_name = 'base'
+
+urlpatterns = [
+    path('', home, name='home'),
+]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def home(request):
+    base_user = request.user
+    user = base_user.candidato
+    return render(request, 'home.html', locals())
