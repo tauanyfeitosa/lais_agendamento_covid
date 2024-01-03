45f9bf3654dc7ee19054c215e90783eb229082d9
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2024-01-03 01:23:57 -0300

    feat (modelos): modelos de GrupoAtendimento e EstabelecimentoSaude

--- base/models.py
+++ base/models.py
@@ -11,3 +11,32 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class GrupoAtendimento(models.Model):
+    nome = models.CharField(max_length=255)
+    visivel = models.BooleanField(default=True)
+    fase = models.CharField(max_length=3)
+    codigo_si_pni = models.CharField(max_length=10)
+    grupo_pai = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    criado_em = models.DateTimeField(auto_now_add=True, editable=False)
+    atualizado_em = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        verbose_name = 'Grupo de Atendimento'
+        verbose_name_plural = 'Grupos de Atendimento'
+
+    def __str__(self):
+        return self.nome
+
+
+class EstabelecimentoSaude(models.Model):
+    nome = models.CharField(max_length=255)
+    codigo_cnes = models.CharField(max_length=7)
+
+    class Meta:
+        verbose_name = u'Estabelecimento de Saúde'
+        verbose_name_plural = u'Estabelecimentos de Saúde'
+
+    def __str__(self):
+        return f"{self.nome} - {self.codigo_cnes}"
