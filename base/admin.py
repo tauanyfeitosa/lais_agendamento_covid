609d58669493ed3accac5544d709e2ba5824ff3b
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2024-01-03 12:59:16 -0300

    feat (estabelecimento_saude): admin para exibir e filtrar informações

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import EstabelecimentoSaude
 
-# Register your models here.
+@admin.register(EstabelecimentoSaude)
+class EstabelecimentoSaudeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'codigo_cnes')
+    search_fields = ('nome', 'codigo_cnes')
+
+    class Meta:
+        model = EstabelecimentoSaude
